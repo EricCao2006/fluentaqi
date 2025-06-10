5049be1a238b441437bbd6ea06b543ba618e65a5
shangyang-360 <2092039930@qq.com>
2025-06-10 16:35:35 +0800

    2025.06.10加了新的依赖，添加service包，写了Aqi的一点，写了users包里的内容

--- src/main/java/com/a360/fluentaqi/back/users/Supervisor.java
+++ src/main/java/com/a360/fluentaqi/back/users/Supervisor.java
@@ -1,9 +1,13 @@
 package com.a360.fluentaqi.back.users;
 
+import java.io.Serializable;
+
 /**
  * 监督员子类
  * @author 尚阳@A360
  */
-public class Supervisor extends User{
+public class Supervisor extends User implements Serializable{
     //todo by 尚阳
+    private static final long serialVersionUID = 1L;
+    private String sex ;
 }
