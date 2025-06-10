89d359d2d1ac891d325642adda42b368618c5006
EricCao2006 <2801132199@qq.com>
2025-06-10 14:49:59 +0800

    2025.06.10 删除了一些依赖

--- src/main/java/com/a360/fluentaqi/LoginRunner.java
+++ src/main/java/com/a360/fluentaqi/LoginRunner.java
@@ -1,7 +1,5 @@
 package com.a360.fluentaqi;
 
-import com.pixelduke.transit.TransitTheme;
-import com.pixelduke.transit.Style;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -17,14 +15,8 @@ public class LoginRunner extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/a360/fluentaqi/front/view.fxml"));
         VBox root = loader.load();
-
         Scene scene = new Scene(root, 720, 540);
-
-        // ✅ 应用 Transit 主题
-        TransitTheme transitTheme = new TransitTheme(Style.LIGHT);
-        transitTheme.setScene(scene);
         scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
-
         stage.setTitle("环保公共监督平台");
         stage.setScene(scene);
         stage.show();
