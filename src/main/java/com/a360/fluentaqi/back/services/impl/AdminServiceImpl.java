1c03c151d57d7cbda52670f11c6f5616eca970b7
shangyang-360 <2092039930@qq.com>
2025-06-11 23:53:42 +0800

    2025.06.11改了AdminService里的路径

--- src/main/java/com/a360/fluentaqi/back/services/impl/AdminServiceImpl.java
+++ src/main/java/com/a360/fluentaqi/back/services/impl/AdminServiceImpl.java
@@ -16,7 +16,7 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public boolean login(String loginCode, String password) throws IOException {
         // TODO Auto-generated method stub
-        String filePath = System.getProperty("user.dir") + "/src/main/resources/com/a360/fluentaqi/back/users.json/";
+        String filePath = System.getProperty("user.dir") + "/src/main/resources/com/a360/fluentaqi/back/users/admins.json/";
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             List<Admin> adminList = objectMapper.readValue(
