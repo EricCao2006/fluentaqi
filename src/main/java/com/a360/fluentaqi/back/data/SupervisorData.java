3c80cdc3ee72a3161c744a5885a2d9292ed52ffe
caigouyimei <2904246570@qq.com>
2025-06-11 20:06:07 +0800

    2025-6-10:完成了JsonReader模块 2025-6-10:修改pom文件 2025-6-10：更新了Jason数据 2025-6-11:更新data包 2025-6-11:完成javafxutil和更新data包

--- src/main/java/com/a360/fluentaqi/back/data/SupervisorData.java
+++ src/main/java/com/a360/fluentaqi/back/data/SupervisorData.java
@@ -1,5 +1,22 @@
 package com.a360.fluentaqi.back.data;
 
-public class SupervisorData {
+import com.a360.fluentaqi.back.users.Supervisor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupervisorData {
+    public static void main(String[] args) {
+        //管理员账号初始化
+        Supervisor s1 = new Supervisor("13812345678","123","张三","男");
+        Supervisor s2 = new Supervisor("13811111111","123","李四","女");
+        Supervisor s3 = new Supervisor("13812341234","123","王五","男");
+
+        List<Supervisor> alist = new ArrayList<Supervisor>();
+        alist.add(s1);
+        alist.add(s2);
+        alist.add(s3);
+        String ProPaht = System.getProperty("user.dir") + "/src/main/resources/NepDatas/ObjectData/";
+        FileUtil.writeObject(ProPaht+"supervisor.txt", alist);
+    }
 }
