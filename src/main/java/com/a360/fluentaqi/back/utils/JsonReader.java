d5819bc1c24f128283aee9217c81f53e00245e82
caigouyimei <2904246570@qq.com>
2025-06-11 21:05:53 +0800

    2025-6-10:完成了JsonReader模块 2025-6-10:修改pom文件 2025-6-10：更新了Jason数据 2025-6-11:更新data包 2025-6-11:完成javafxutil和更新data包 2025-6-11:修data里的bug并导入json数据

--- src/main/java/com/a360/fluentaqi/back/utils/JsonReader.java
+++ src/main/java/com/a360/fluentaqi/back/utils/JsonReader.java
@@ -15,69 +15,6 @@ import java.util.List;
  * json文件读写
  * @author 夏从耀@A360
  */
-//public class JsonReader {
-//    // 获取当前项目类路径（读取json文件的时候会用到，对象格式数据用不到此对象）
-//    public static ClassLoader classLoader = JsonReader.class.getClassLoader();
-//    // 操作json格式数据时用的工具类对象（操作json文件的时候会用到，对象格式数据用不到此对象）
-//    public static ObjectMapper objectMapper = new ObjectMapper();
-//
-//
-//    // 用Jackson工具类读取json格式类型数据时，参照此方法内容
-//    public static List<Admin> getListByJackson() {
-//
-//        List<Admin> admin = new ArrayList<>();
-//        try {
-//            // 通过类加载器加载json格式文件内容
-//            InputStream inputStream = classLoader.getResourceAsStream("com/a360/fluentaqi/back/users/admins.json");
-//            // 将JSON数组字符串转换为Employee对象列表
-//            admin = objectMapper.readValue(inputStream, new TypeReference<List<Admin>>() {
-//            });
-//
-//            // 打印解析后的对象列表
-//            for (Admin employee : admin) {
-//                System.out.println(employee);
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return admin;
-//    }
-//
-//    // 用Jackson工具类写入json格式类型数据时，参照此方法内容
-//    public static void writeListByJson_admin(List<Admin> admins) {
-//        try {
-//            // 获取输出流，指向类路径下的目标文件
-//            OutputStream outputStream =
-//                    new FileOutputStream(JsonReader.class.getClassLoader().getResource("com/a360/fluentaqi/back/users/admins.json").getFile());
-//
-//
-//            // 下面的代码适用于创建新文件或覆盖现有文件的情况。
-//            // 如果你想追加数据，或者处理文件不存在的情况，需要做相应的逻辑调整。
-//
-//            // 使用ObjectMapper将Employee对象列表序列化为JSON字符串，并写入输出流
-//            objectMapper.writeValue(outputStream, admins);
-//
-//            // 关闭输出流
-//            outputStream.close();
-//
-//            System.out.println("数据写入成功.");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        // 调用读取json格式数据方法
-//        List<Admin> admins = getListByJackson();
-//        admins.add(new Admin(3, "admin", "111",null));
-//        // 调用写入json格式数据方法
-//        writeListByJson_admin(admins);
-//    }
-//}
-
-
 
 public class JsonReader {
     // 线程安全的ObjectMapper实例
