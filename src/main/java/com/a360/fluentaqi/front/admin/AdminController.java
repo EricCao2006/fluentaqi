61ebfdac1a9220603495b13e6e690503b3acfc25
zhanshenad <3697233161@qq.com>
2025-06-11 18:40:32 +0800

    view.fxml

--- src/main/java/com/a360/fluentaqi/front/admin/AdminController.java
+++ src/main/java/com/a360/fluentaqi/front/admin/AdminController.java
@@ -1,4 +1,24 @@
 package com.a360.fluentaqi.front.admin;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AdminController {
+
+    @FXML
+    void aqiAssign(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aqiConfirm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aqiInfo(ActionEvent event) {
+
+    }
+
 }
+
