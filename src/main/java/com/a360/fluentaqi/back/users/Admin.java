d0e2bfb969e401d428a675cc11b5a3c04fde8604
shangyang-360 <2092039930@qq.com>
2025-06-15 15:34:25 +0800

    2025.06.15写了下Admin_id

--- src/main/java/com/a360/fluentaqi/back/users/Admin.java
+++ src/main/java/com/a360/fluentaqi/back/users/Admin.java
@@ -7,9 +7,14 @@ import java.io.Serializable;
  * @author 尚阳@A360
  */
 public class Admin extends User implements Serializable {
-    //todo by 尚阳
     private static final long serialVersionUID = 1L;
-
+    private String Admin_id;
+    public String getAdmin_id(){
+        return Admin_id;
+    }
+    public String setAdmin_id(){
+        this.Admin_id = Admin_id;
+    }
     @Override
     public String toString() {
         return "Admin [getLoginCode()=" + getLoginCode() + ", getPassword()=" + getPassword() + ", getRealName()="
